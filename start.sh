b686bb1fbf2792054f76e940f54487aeaabb7da5
ben.marengo <ben.marengo@just-eat.com>
2020-03-21 11:29:20 +0000

    fix docker compose

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ set +e
 
 set -m
 trap 'list=$( jobs -rp ); test -n "$list" && kill $list' CHLD
-python -um rakomqtt "${RAKO_BRIDGE_IP}" "$@" &
+python -um rakomqtt --mode watcher "$@" &
 python -um rakomqtt --mode commander --rako-bridge-host "${RAKO_BRIDGE_IP}" "$@" &
 wait
